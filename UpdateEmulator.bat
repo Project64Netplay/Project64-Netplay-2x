1be0ad1de1572a17800be34a63ac35851938581d
Cindy Lou Who <newer2team@gmail.com>
2018-12-31 19:24:40 -0500

    Update UpdateEmulator.bat

--- UpdateEmulator.bat
+++ UpdateEmulator.bat
@@ -10,6 +10,15 @@ goto :Exit
 TASKKILL /IM Project64.exe /F
 wget.exe https://github.com/CindyLouWho03/MarioEverything-Project64Netplay/archive/master.zip
 7z x master.zip
+mkdir BackupCache
+cd Config
+move Project64.cfg ..\
+move NRage.ini ..\
+cd ..\
+cd Plugin
+cd GFX
+move GlideN64.ini ..\..\
+cd ..\..\
 rmdir Config /s
 rmdir Extras /s
 rmdir Plugin /s
@@ -25,6 +34,9 @@ move Replace.bat ..\
 cd ..\
 rmdir MarioEverything-Project64Netplay-master /s
 DEL master.zip
+move GlideN64.ini Plugin\GFX
+move NRage.ini Config
+move Project64.cfg Config
 start Project64.exe
 
 :Exit
