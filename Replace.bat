77137b42bf56f447cfe41145e5137cad0ce9c42a
Cindy Lou Who <newer2team@gmail.com>
2019-01-01 16:39:33 -0500

    Test

--- Replace.bat
+++ Replace.bat
@@ -1,4 +1,5 @@
 @echo off
+Debug
 title Mario Everything Project 64 Save Replacer
 echo ===============================================================================
 fc /b "Save\Backup\MarioParty.eep" "Save\MarioParty.eep" > nul
