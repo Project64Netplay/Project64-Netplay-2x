84a8dab51986664f6c6ab61bd3e48281e44c9ee1
Cranky Supertoon <48419040+CrankySupertoon@users.noreply.github.com>
2019-12-30 19:27:24 -0500

    mega push

--- Source/Project64-core/Version.h
+++ Source/Project64-core/Version.h
@@ -16,7 +16,7 @@
 
 #define VER_FILE_DESCRIPTION_STR    "Project64 Netplay"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
-#define VER_FILE_VERSION_STR        "Netplay - (December 1, 2019)"
+#define VER_FILE_VERSION_STR        "Netplay - (December 30, 2019)"
 
 
 #define VER_PRODUCTNAME_STR         "Project64 Netplay"
